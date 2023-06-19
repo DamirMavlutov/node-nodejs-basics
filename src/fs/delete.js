046fa9c9ed81a473ce96b5a4bb4e3f9cb1a383a6
Damir <DM936420@yandex.ru>
2023-06-19 21:14:01 +0400

    init: delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+const fs = require("fs");
 const remove = async () => {
-    // Write your code here 
+  try {
+    const wrongFile = "./files/wrongFilename.txt";
+
+    if (!fs.existsSync(wrongFile)) {
+      throw new Error("FS operation failed");
+    }
+    await fs.promises.unlink(wrongFile);
+    console.log("Файл удален");
+  } catch (err) {
+    if (err) throw err; // не удалось переименовать файл
+    console.log("Файл успешно переименован");
+  }
 };
 
-await remove();
+remove();
