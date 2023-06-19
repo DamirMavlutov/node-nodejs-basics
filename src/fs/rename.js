c5f9701c7ab643144a127a634738e335903a0a58
Damir <DM936420@yandex.ru>
2023-06-19 20:56:48 +0400

    init: rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+const fs = require("fs");
 const rename = async () => {
-    // Write your code here 
+  try {
+    const wrongFile = "./files/wrongFilename.txt";
+    const newFile = "./files/wrongFilename.md";
+    if (!fs.existsSync(wrongFile) || fs.existsSync(newFile)) {
+      throw new Error("FS operation failed");
+    }
+    await fs.promises.rename(wrongFile, newFile);
+    console.log("Файл переименован");
+  } catch (err) {
+    if (err) throw err; // не удалось переименовать файл
+    console.log("Файл успешно переименован");
+  }
 };
-
-await rename();
+rename();
