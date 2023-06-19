bfc8080609359fea655b11608591a7512fb5dfeb
Damir <DM936420@yandex.ru>
2023-06-19 18:54:46 +0400

    init: copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
-const copy = async () => {
-    // Write your code here 
-};
+const fs = require("fs");
+const path = require("path");
 
-await copy();
+async function copyFiles() {
+  const sourceDir = "files";
+  const targetDir = "files_copy";
+
+  if (!fs.existsSync(sourceDir) || fs.existsSync(targetDir)) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    fs.mkdirSync(targetDir);
+
+    const files = fs.readdirSync(sourceDir);
+    for (let index = 0; index < files.length; index++) {
+      const file = files[index];
+
+      const sourcePath = path.join(sourceDir, file);
+      const targetPath = path.join(targetDir, file);
+
+      await fs.promises.copyFile(sourcePath, targetPath);
+    }
+
+    console.log("Files copied successfully.");
+  } catch (error) {
+    console.error("FS operation failed:", error);
+  }
+}
+
+copyFiles();
